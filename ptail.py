94612fd6a3f7a494e92d3851aeb634c57d5bc660
Dave Cremins <dave.cremins@wpengine.com>
2017-12-11 20:37:38 +0000

    Add function to open file

--- ptail.py
+++ ptail.py
@@ -1,8 +1,15 @@
 #!/usr/local/bin/python
 
+import sys
+
 def tail(file_handle):
-    print('Not implemented yet')
+    read_result = file_handle.read()
+    print(read_result)
+
+def connect_to_file(file_name):
+    with open(file_name, 'r') as file:
+        tail(file)
 
 if __name__ == '__main__':
-    f = ''
-    tail(f)
+    file_name = sys.argv[1]
+    connect_to_file(file_name)
