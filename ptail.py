82217fcacf6e193de35f608a5011092a682cc5e2
Dave Cremins <dave.cremins@wpengine.com>
2017-12-11 21:32:32 +0000

    Use decorator to continously tail

--- ptail.py
+++ ptail.py
@@ -1,30 +1,46 @@
 #!/usr/local/bin/python
 
 import sys
-import argparse
+import time
 
 default_character_line_size = 75
 default_line_count = 10
+file_position = None
+interval = .500
 
-def get_read_position(file_size):
-    return file_size - (default_character_line_size*default_line_count)
+def execute_and_sleep(secs):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            while True:
+                func(*args, **kwargs)
+                time.sleep(secs) 
+        return wrapper
+    return decorator
+
+def get_read_position(file_position):
+    return file_position - (default_character_line_size*default_line_count)
 
 def show(lines):
     formatted = ''.join(lines)
     print(formatted)
 
+@execute_and_sleep(interval)
 def tail(file_handle):
     file_handle.seek(0, 2)
     file_position = file_handle.tell()
     read_position = get_read_position(file_position)
     file_handle.seek(read_position, 0)
     lines = file_handle.readlines()
+    file_position = file_handle.tell()
     lines = lines[-10:]
     show(lines)
 
 def connect_to_file(file_name):
-    with open(file_name, 'r') as file:
-        tail(file)
+    try:
+        with open(file_name, 'r') as file:
+            tail(file)
+    except KeyboardInterrupt:
+        exit()
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
