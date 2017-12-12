b0caf0be106f9e246c98f4e11af84a4313a5a88f
Dave Cremins <dave.cremins@wpengine.com>
2017-12-12 00:00:57 +0000

    Remove bad decorator and inner loop

--- ptail.py
+++ ptail.py
@@ -6,21 +6,11 @@ import time
 DEFAULT_CHARACTER_LINE_SIZE = 75
 DEFAULT_LINE_COUNT = 10
 MAX_SIZE = DEFAULT_CHARACTER_LINE_SIZE*DEFAULT_LINE_COUNT
-INTERVAL = .5
-
-def execute_and_sleep(secs):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            while True:
-                func(*args, **kwargs)
-                time.sleep(secs) 
-        return wrapper
-    return decorator
+INTERVAL = 1
 
 def attach(data):
     sys.stdout.write(data)
 
-@execute_and_sleep(INTERVAL)
 def tail(file_handle):
     last_file_position = 0
     last_seek_position = 0
@@ -35,7 +25,7 @@ def tail(file_handle):
             data = file_handle.read()
             last_file_position = file_handle.tell()
             attach(data)
-
+        time.sleep(INTERVAL) 
 
 def connect_to_file(file_name):
     try:
@@ -46,6 +36,9 @@ def connect_to_file(file_name):
     except KeyboardInterrupt:
         exit()
 
-if __name__ == '__main__':
-    file_name = sys.argv[1]
-    connect_to_file(file_name)
+if(__name__ == '__main__'):
+    try:
+        file_name = sys.argv[1]
+        connect_to_file(file_name)
+    except IndexError:
+        exit('ptail requires a path to a file as an argument! Exiting...')
